fab3c3cb3c9854f14b5b1ba2257d9da2bc236b52
GuoqingChen <guoqing.chen01@hand-china.com>
2023-12-19 10:56:11 +0800

    jsonFormat工具完成

--- main.go
+++ main.go
@@ -1,46 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"go_tools/consoles"
 	"go_tools/tools"
-	"log"
-
-	"github.com/eiannone/keyboard"
+	"os"
 )
 
+// func main() {
+// 	tools.ClearConsole()
+// 	// 持续接收控制台的输入
+// 	// 检测用户的键盘按键，按下键盘上的esc退出
+// 	err := keyboard.Open()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	for {
+// 		fmt.Println("------------------------------")
+// 		fmt.Printf("%s\n%s\n\n%s\n%s\n", "支持的命令(请输入编号):", "1. jsonFormat", "快捷命令:", "jof: [default]jsonFormat(将全部使用默认值,从默认文件(Documents\\json-in.json)读取并输出到默认文件(Documents\\json-out.json))")
+// 		if tools.MonitoringKeyboard("请输入命令编号(jof):", keyboard.KeyEsc) {
+// 			return
+// 		}
+// 		var input string
+// 		fmt.Scanln(&input)
+// 		switch input {
+// 		case "1":
+// 			if consoles.JsonFormatFromConsole("") {
+// 				return
+// 			}
+// 			//  else {
+// 			// 	consoles.ClearConsole()
+// 			// }
+// 		case "jof":
+// 		default:
+// 			input = "jof"
+// 			if consoles.JsonFormatFromConsole(input) {
+// 				return
+// 			} else {
+// 				tools.ClearConsole()
+// 			}
+// 		}
+// 	}
+// }
+
 func main() {
-	tools.ClearConsole()
-	// 持续接收控制台的输入
-	// 检测用户的键盘按键，按下键盘上的esc退出
-	err := keyboard.Open()
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		fmt.Println("------------------------------")
-		fmt.Printf("%s\n%s\n\n%s\n%s\n", "支持的命令(请输入编号):", "1. jsonFormat", "快捷命令:", "jof: [default]jsonFormat(将全部使用默认值,从默认文件(Documents\\json-in.json)读取并输出到默认文件(Documents\\json-out.json))")
-		if tools.MonitoringKeyboard("请输入命令编号(jof):", keyboard.KeyEsc) {
-			return
-		}
-		var input string
-		fmt.Scanln(&input)
-		switch input {
-		case "1":
-			if consoles.JsonFormatFromConsole("") {
-				return
-			}
-			//  else {
-			// 	consoles.ClearConsole()
-			// }
-		case "jof":
-		default:
-			input = "jof"
-			if consoles.JsonFormatFromConsole(input) {
-				return
-			} else {
-				tools.ClearConsole()
-			}
-		}
-	}
+	// 读取用户home路径
+	userHome := os.Getenv("USERPROFILE")
+	var basePath string = userHome + "\\Documents"
+	var inFilePath string = basePath + "\\json-in.json"
+	var outFilePath string = basePath + "\\json-out.json"
+	tools.JsonFormatFromFile(inFilePath, outFilePath)
 }
--- tools/jsonFormat.go
+++ tools/jsonFormat.go
@@ -1,20 +1,67 @@
 package tools
 
 import (
+	"bufio"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
+	"unicode/utf8"
 )
 
 func JsonFormatFromConsole(jsonStr string) string {
+	// 使用正则表达式判断字符传的加密类型是否为base64
+	regex := regexp.MustCompile(`^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$`)
+	if regex.MatchString(jsonStr) {
+		fmt.Println("字符串符合Base64编码规则")
+		// base64解码
+		decodeBytes, err := base64.StdEncoding.DecodeString(jsonStr)
+		if err != nil {
+			fmt.Println("base64解码失败:", err)
+		}
+		jsonStr = string(decodeBytes)
+	}
+	// 处理字符串中可能有的base64字符串片段
+	base64SubStringRegex := regexp.MustCompile(`[A-Za-z0-9+/]+={1,2}`)
+	encodedStrings := base64SubStringRegex.FindAllString(jsonStr, -1)
+	// 遍历encodedStrings
+	for _, encodedString := range encodedStrings {
+		// base64解码
+		decodeBytes, err := base64.StdEncoding.DecodeString(encodedString)
+		if err == nil {
+			// base64解码后的字符串
+			jsonStrByDecode := string(decodeBytes)
+			jsonStrByDecode = strings.ReplaceAll(jsonStrByDecode, `"`, `\"`)
+			jsonStrByDecode = strings.ReplaceAll(jsonStrByDecode, `{`, `"{`)
+			// jsonStrByDecode = strings.ReplaceAll(jsonStrByDecode, `}`, `}"`)
+			jsonStrByDecode = strings.ReplaceAll(jsonStrByDecode, `[`, `"[`)
+			jsonStrByDecode = strings.ReplaceAll(jsonStrByDecode, `]`, `]"`)
+			// 替换字符串
+			// fmt.Println("encodedString: ", encodedString, "jsonStrByDecode: ", jsonStrByDecode)
+			if utf8.ValidString(jsonStrByDecode) {
+				jsonStr = strings.ReplaceAll(jsonStr, encodedString, jsonStrByDecode)
+			}
+		}
+	}
 	s := strings.ReplaceAll(jsonStr, `\"`, `"`)
+	s = strings.ReplaceAll(s, `"{`, `{`)
+	s = strings.ReplaceAll(s, `}"`, `}`)
 	s = strings.ReplaceAll(s, `"{"`, `{"`)
 	s = strings.ReplaceAll(s, `"}"`, `"}`)
+	s = strings.ReplaceAll(s, `"[{`, `[{`)
+	s = strings.ReplaceAll(s, `}]"`, `}]`)
 	return s
 }
 
 func JsonFormatFromConsoleForPrettyJson(jsonStr string) []byte {
+	// 使用正则表达式校验jsonStr是否为json格式
+	regex := regexp.MustCompile(`^\s*{.*}\s*$`)
+	if !regex.MatchString(jsonStr) {
+		fmt.Println("不是json格式字符串")
+		return nil
+	}
 	// 使用json格式化json格式数据s
 	s := JsonFormatFromConsole(jsonStr)
 	// 解析JSON字符串
@@ -22,6 +69,7 @@ func JsonFormatFromConsoleForPrettyJson(jsonStr string) []byte {
 	err := json.Unmarshal([]byte(s), &data)
 	if err != nil {
 		fmt.Println("解析JSON失败:", err)
+		fmt.Println("JSONString:", s)
 	}
 
 	// 格式化JSON
@@ -44,7 +92,6 @@ func JsonFormatFromFile(inFilePath string, outFilePath string) {
 		outFilePath = "C:\\Temp\\test-out.txt"
 	}
 	// 读取filePath文件的第一行内容
-	jsonStr := ""
 	// 判断inFilePath文件是否存在,如果不存在则创建
 	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
 		_, err := os.Create(inFilePath)
@@ -59,14 +106,48 @@ func JsonFormatFromFile(inFilePath string, outFilePath string) {
 			panic(err)
 		}
 	}
-	f, err := os.ReadFile(inFilePath)
-	// handle err
+	// 读取文件
+	file, err := os.Open(inFilePath)
 	if err != nil {
-		panic(err)
+		fmt.Println("打开文件错误:", err)
+		return
 	}
-	// 将f转为string赋值给jsonStr
-	jsonStr = string(f)
-	prettyJSON := JsonFormatFromConsoleForPrettyJson(jsonStr)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	outputString := ""
+	for {
+		line, err := reader.ReadString('\n')
+
+		// 判断换行符
+		switch {
+		case err == nil:
+			// 行末尾有换行符
+			// fmt.Println("行:", line)
+			outputString = readLineAndHandle(outputString, line)
+		case err.Error() == "EOF":
+			// 文件结束，最后一行可能没有换行符
+			// fmt.Println("行:", line)
+			outputString = readLineAndHandle(outputString, line)
+			os.WriteFile(outFilePath, []byte(outputString), 0644)
+			// fmt.Println("文件结束")
+			return
+		default:
+			// 处理读取错误
+			fmt.Println("读取错误:", err)
+			return
+		}
+	}
+}
+
+func readLineAndHandle(outputString string, line string) string {
+	if line == "" {
+		return outputString
+	}
+	prettyJSON := JsonFormatFromConsoleForPrettyJson(line)
 	// 将s写入outFilePath文件
-	os.WriteFile(outFilePath, []byte(prettyJSON), 0644)
+	outputString += string(prettyJSON)
+	outputString += "\n"
+	outputString += "\n"
+	return outputString
 }
